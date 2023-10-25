64897539e8adc2f8c0ac41104a7dcd2faa32d1c9
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-25 14:00:08 +0530

    Update and rename How to find area of circle to How to find area of circle.py

---
+++ How to find area of circle.py
@@ -0,0 +1,44 @@
+#will discuss the progrm to find area of circle using python .Area of circle is the number of square units inside the circle , can be calculated using the radius and diameter.
+#The formula for evaluating the area of circle is
+#Area of circle using radius=   Pi*r*r    ( r is the radius of circle)
+#Area of circle using diameter=  1/4*Pi*d*d   ( d is the diameter of circle)
+ 
+
+#Area of circle using python
+
+Algorithm
+import pi from math
+Set r = 3
+Set are = pi*r*r
+Print(area)
+
+'''Code in Python'''
+
+from math import pi
+r=3.00 
+area=pi*r*r 
+print("The area of circle is",end=" ") 
+print(area)
+
+Output :
+The area of circle is 28.27
+
+Using Diameter
+
+import pi from math
+Set d = 3
+Set area = (pi*d*d)/4
+Print(area)
+
+
+'''Code in Python'''
+
+from math import pi
+d=6.00
+area=(pi*d*d)/4
+print("The area of circle is",end="")
+print(area)
+
+
+Output :
+The area of circle is 28.27
